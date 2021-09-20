0265dd8f9e5454c84fce842a43dd65f793dc95d6
jainayaan1608 <82639400+jainayaan1608@users.noreply.github.com>
2021-09-20 10:05:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet');
+}
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("object_name").innerHTML = results[0].label;
+    
+  }
+}
